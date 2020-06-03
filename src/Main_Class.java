3d71866f200b5c93c2aacceece2f3d28c76f6a7b
Davidaguirre99 <Davidaguirre99@users.noreply.github.com>
2020-06-03 13:15:10 -0500

    funcion sacarmodulo
    mijin ojala no de conflicto

--- src/Main_Class.java
+++ src/Main_Class.java
@@ -1,6 +1,10 @@
 
 public class Main_Class {
-
+        public double sacarmodulo(float x, float y){
+            double m = Math.pow(x, 2);
+            m += Math.pow(y, 2);
+            return m;
+        }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println( "Arithmetic Operations");
