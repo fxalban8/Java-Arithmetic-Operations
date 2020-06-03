a11b1e2cce66e6ce71f4c98e31a9366e4ba6549d
fxalban8 <francisco.alban@epn.edu.ec>
2020-06-03 12:44:20 -0500

    Showing the values to add, substract, multiplicate and divide

---
+++ src/Main_Class.java
@@ -0,0 +1,10 @@
+
+public class Main_Class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println( "Arithmetic Operations");
+		System.out.println("x=5 y=6");
+	}
+
+}
